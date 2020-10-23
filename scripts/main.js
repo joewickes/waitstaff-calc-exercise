5787cacfde056bc6daa8908691330a139d234cc0
Joe Wickes <joe.c.wickes@gmail.com>
2020-10-23 12:47:20 -0400

    Basic functionality working

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,127 @@
 import store from './store.js';
+
+function render() {
+  $('main').html(`
+    <div class="all-boxes">
+      <div class="left-side">
+        <div class="meal-details-container">
+          <div class="box-header">
+            <h2>Meal Details</h2>
+          </div>
+          <div class="details">
+            <form id="meal-details-form">
+              <div>
+                <label for="base-meal-price">Base Meal Price $</label>
+                <input type="number" name="base-meal-price" id="base-meal-price">
+              </div>
+              <div>
+                <label for="tax-rate">Tax Rate %</label>
+                <input type="number" name="tax-rate" id="tax-rate">
+              </div>
+              <div>
+                <label for="tip-percentage">Tip Percentage %</label>
+                <input type="number" name="tip-percentage" id="tip-percentage">
+              </div>
+            </form>
+            <div class="buttons">
+              <button type="submit" form="meal-details-form" id="form-button">Submit</button>
+              <button id="cancel-button">Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="right-side">
+        <div class="customer-charges-container">
+          <div>
+            <h2>Customer Charges</h2>
+          </div>
+          <div class="charges">
+            <div class="whole-charges">
+              <div class="left-charges">
+                <p>Subtotal</p>
+                <p>Tip</p>
+              </div>
+              <div class="right-charges">
+                <p>${store.subTotal}</p>
+                <p>${store.tip}</p>
+              </div>
+            </div>
+            <div class="whole-total">
+              <div class="left-total">
+                <p>Total</p>
+              </div>
+              <div class="right-total">
+                <p>${store.total}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <div class="my-earnings-info-container">
+          <div>
+            <h2>My Earnings Info</h2>
+          </div>
+          <div class="earnings">
+            <div class="left-earnings">
+              <p>Tip Total</p>
+              <p>Meal Count</p>
+              <p>Average Tip Per Meal</p>
+            </div>
+            <div class="right-earnings">
+              <p>${store.tipTotal}</p>
+              <p>${store.mealCount}</p>
+              <p>${store.avgTPM}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `);
+}
+
+function handleSubmit() {
+  $('main').on('submit', '#meal-details-form', function(e) {
+    e.preventDefault();
+    const bmpVal = $('#base-meal-price').val();
+    const trVal = $('#tax-rate').val();
+    const tpVal = $('#tip-percentage').val();
+
+    if (validateNums(bmpVal, trVal, tpVal)) {
+      const bmp = parseFloat(bmpVal);
+      const tr = parseFloat(trVal) / 100;
+      const tp = parseFloat(tpVal) / 100;
+      store.details.push(store.createItem(bmp, tr, tp));
+      store.getSubtotal();
+      store.getTip();
+      store.getTotal();
+      store.mealCount++;
+      store.getAvgTPM();
+      render();
+    } else {
+      throw new Error('Please fill out all areas');
+    }
+  });
+
+}
+
+function validateNums(bmp, tr, tp) {
+  const arr = [bmp, tr, tp];
+
+  for (let i = 0; i < arr.length; i++) {
+    const currentParse = parseFloat(arr[i]);
+    if (isNaN(currentParse)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+function main() {
+  render();
+  handleSubmit();
+}
+
+$(main);
--- scripts/store.js
+++ scripts/store.js
@@ -1,6 +1,48 @@
-
-
-
-export default {
+const store = {
+  details: [{bmr: 10, tr: .33, tp: .2}],
+  createItem: function(bmp, tr, tp) {
+    return {
+      id: cuid(),
+      bmp: bmp,
+      tr: tr,
+      tp: tp,
+    };
+  },
+  subTotal: 0.00,
+  tip: 0.00,
+  total: 0.00,
+  tipTotal: 0.00,
+  mealCount: 0,
+  avgTPM: 0.00,
+  getSubtotal: function() {
+    const set = this.details[this.details.length - 1];
+    this.subTotal = set.bmp + (set.bmp * set.tr);
+  },
+  getTip: function() {
+    const set = this.details[this.details.length - 1];
+    console.log(set);
+    const tip = set.bmp * set.tp;
+    this.tip = tip;
+    this.tipTotal += tip;
+  },
+  getTotal: function() {
+    this.total = this.subTotal + this.tip;
+  },
+  getAvgTPM: function() {
+    this.avgTPM = this.tipTotal / this.mealCount;
+  },
   
 };
+
+export default store;
+
+// export default {
+//   details,
+//   createItem,
+//   mealCount,
+//   subTotal,
+//   tip,
+//   total,
+//   tipTotal,
+//   avgTPM,
+// };
